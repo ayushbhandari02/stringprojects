2657ce49c899bcd716150de1025a74ba37e0800f
Ayush Bhandari <ayushbhandari02@gmail.com>
2019-07-11 21:27:42 +0530

    finding common prefix

---
+++ find_commonprefix.py
@@ -0,0 +1,36 @@
+list = []
+while (1):
+    try:
+        list.append(input('enter the new element'))
+    except exception:
+        print("enter the valid input")
+    if input('enter x to exit or enter to continue appending names') == 'x':
+        break
+    else:
+        continue
+min_length = len(min(list, key=len))
+print(min_length)
+length_list = len(list)
+print(length_list)
+flag = 0
+common_index = -1
+for i in range(min_length):
+
+    for j in range(length_list - 1):
+        print(j)
+        if list[0][i] == list[j + 1][i]:
+            continue
+        else:
+            flag = 1
+    if flag == 1:
+        break
+    common_index = i
+print(common_index)
+if common_index == -1:
+    print("no  prefix found")
+for k in range(common_index+1):
+    print(list[0][k], end=" ")
+if common_index == 0:
+    print(" is the common prefix")
+if common_index > 0:
+    print("are the common prefix")
---
+++ last_word.py
@@ -0,0 +1,3 @@
+string = input("enter the string")
+list = string.split(" ", 2)
+print(list)
